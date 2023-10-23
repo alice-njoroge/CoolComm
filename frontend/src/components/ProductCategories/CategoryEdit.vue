d827b7b07c3c0a070acbbdbda55009415c7a3e1d
Alice Wanjiku <alicenjoroge707@gmail.com>
2023-10-23 16:59:29 +0300

    UX improvements if a user tries to access/edit a category that does not exist

--- frontend/src/components/ProductCategories/CategoryEdit.vue
+++ frontend/src/components/ProductCategories/CategoryEdit.vue
@@ -2,6 +2,7 @@
 import axios from "axios";
 import Form from "@/components/utils/Form.vue";
 import Button from "@/components/utils/Button.vue";
+import NotFound from "@/components/utils/NotFound.vue";
 
 export default {
   name: "CategoryEdit",
@@ -20,7 +21,8 @@ export default {
         name:"",
         description:""
       },
-      submitting: false
+      submitting: false,
+      categoryFound: false
     }
   },
   methods:{
@@ -34,8 +36,13 @@ export default {
   },
 
   async created() {
-    const response = await axios.get(`http://api.backend.orb.local/products/categories/${this.id}`);
-    this.category = response.data;
+   try {
+     const response = await axios.get(`http://api.backend.orb.local/products/categories/${this.id}`);
+     this.category = response.data;
+     this.categoryFound = true
+   }catch (e) {
+     console.log("error", e)
+   }
 
     //prefill the form with category data
     this.form.name = this.category.name;
@@ -45,13 +52,19 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div v-if="categoryFound">
     <h3 class="text-center mb-2"> Edit Category {{category.id}}</h3>
     <Form :form="this.form"></Form>
     <Button name="Save Changes" @click="editCategory" :submitting="submitting"></Button>
     <Button  @click="$router.back()" name="Cancel" class="btn btn-secondary"/>
     <!-- TODO: how can I delete from here?? would love to open a confirmation modal first -->
     <Button class="btn btn-danger" name="Remove Category"/>
+  </div>
+  <div v-else>
+    <p> ooopsie, item not found! you can try going back to
+      <router-link to="/categories">categories</router-link>
+      page and select an item again
+    </p>
   </div>
 </template>
 
